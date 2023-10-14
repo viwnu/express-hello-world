123fcf20d7d433d61bab63f465355e8d485f3718
viwnu <xenmysafitiplace@gmail.com>
2023-10-14 15:35:00 +0300

    set insetead list()

--- db/db.js
+++ db/db.js
@@ -1,15 +1,12 @@
 const CyclicDb = require("@cyclic.sh/dynamodb")
 
-console.log('CyclicDb: ', CyclicDb)
-
 const db = CyclicDb("tiny-blue-hatchling-robeCyclicDB") // find it on the Database/Storage tab
-console.log('db: ', db)
 let agregations = db.collection('agregations')
 
 console.log('agregations: ', agregations)
 
 module.exports.readBundle = async (filePath) => {
-    const bundle = await agregations.list()
+    const bundle = await agregations.get('agregations')
     console.log('in readBundle', bundle)
     return bundle
 }
@@ -19,7 +16,7 @@ module.exports.writeBundle = async (filePath, data) => {
     await agregations.set(data.id, data)
 }
 
-module.exports.deleteBundle = async (filePath, id) => {
-    console.log('in deleteBundle', id)
-    await agregations.delete(id)
-}
+// module.exports.deleteBundle = async (filePath, id) => {
+//     console.log('in deleteBundle', id)
+//     await agregations.delete(id)
+// }
